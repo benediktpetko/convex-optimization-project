8a1b00fc479300c8e1ff4ae95433749539097782
Benedikt Petko <benedikt.petko@gmail.com>
2021-02-19 22:22:08 +0100

    Update

--- example.py
+++ example.py
@@ -2,17 +2,15 @@ import numpy as np
 import ot
 import matplotlib.pyplot as plt
 
-from sklearn.datasets import load_digits
+from sklearn.datasets import load_digits, fetch_openml
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import normalize
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
-from ot.utils import clean_zeros
-from utils import compute_sinkhorn_divergence, compute_emd
 
 
 X, y = load_digits(return_X_y=True)
+#Uncomment below to load MNIST and comment out the above line
+#X, y = fetch_openml('mnist_784', return_X_y=True, as_frame=False)
 
 # normalize pixel intensities
 X = normalize(X, axis=1, norm='l1')
@@ -21,17 +19,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
 # pixel coordinate array
 p = np.mgrid[0:8:1, 0:8:1].reshape(2,-1).T
+#Uncomment below to use bigger cost matrix for MNIST and comment out the above line
+#p = np.mgrid[0:64:1, 0:64:1].reshape(2,-1).T
 
 # cost matrix with Euclidean base metric
 M = ot.dist(p,  metric='euclidean')
-
-knn = KNeighborsClassifier()
-knn.fit(X_train, y_train)
-y_pred = knn.predict(X_test)
-ac = accuracy_score(y_test, y_pred)
-breakpoint()
-# example: Sinkhorn divergence of the first two images with lambda = 1e-3
-#print(compute_sinkhorn_divergence(X[0,:], X[1,:], M, reg=1e-2))
-
-# example: compute EMD of the first two images
-#print(compute_emd(X[0,:], X[1,:], M))
